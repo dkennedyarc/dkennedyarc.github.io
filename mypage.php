ee125619bf88e004ae70e8d2399ef6ce6712439b
dkennedyarc <dkennedy@wesleyan.edu>
2016-10-05 22:19:44 -0400

    Add files via upload

---
+++ mypage.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>FUCKIN SICK</title>
+<style>
+body {
+
+	background-color: black;
+}
+
+.alert {
+
+	color: blue;
+}
+
+input {
+	
+	background:none!important;
+     border:none; 
+     padding:0!important;
+     font: inherit;
+     color: blue;
+     font-size: 40;
+     /*border is optional*/
+     border-bottom:1px solid #444; 
+     cursor: pointer;
+}
+</style>
+<script>
+
+	function FuckingSick() {
+	
+		alert("FUCKIN SICK");
+	}
+</script>
+<body>
+
+	<input type='button' value='FUCKIN SICK' onClick='FuckingSick()'/>
+	<p class='alert'>FUCKIN SICK</p>
+	
+	<?php
+	
+		echo "Are niggers even human?";
+		
+	?>	
+</body>
+</html>
